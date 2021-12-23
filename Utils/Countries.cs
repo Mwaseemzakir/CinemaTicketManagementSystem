c46cf99d868244fda0597f851daa64092df8d25b
Mwaseemzakir <Mwaseemzakir@gmail.com>
2021-12-23 16:21:34 +0500

    Added All Countries using CountryData.Standard built in Extension of .NET

---
+++ Utils/Countries.cs
@@ -0,0 +1,15 @@
+﻿using CountryData.Standard;
+using System.Collections.Generic;
+
+namespace CinemaTicketManagementSystem.Utils
+{
+    public static class Countries
+    {
+       public static List<string> GetAll()
+        {
+            var helper = new CountryHelper();
+            var countries = helper.GetCountries();
+            return countries;
+         }
+    }
+}
